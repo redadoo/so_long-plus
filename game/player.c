96b62004f3d9392e9a581ed22c620073a484efe5
edoardo <edoardovocaturo@gmail.com>
2023-04-05 17:59:52 +0200

    c

--- game/player.c
+++ game/player.c
@@ -6,7 +6,7 @@
 /*   By: evocatur <evocatur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 14:57:00 by evocatur          #+#    #+#             */
-/*   Updated: 2023/04/05 14:46:58 by evocatur         ###   ########.fr       */
+/*   Updated: 2023/04/05 17:58:32 by evocatur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_player set_player(t_program program,char **map)
     int		 	img_width;
 	int		 	img_height;
 	vector2 	Player_start_pos;
-
+	t_data enemy;
 	Player_start_pos = obj_pos('P',map);
     player.sprite.img = mlx_xpm_file_to_image(program.mlx, "Asset/player/player1.xpm", &img_width, &img_height);
 	mlx_put_image_to_window(program.mlx, program.window.reference,  player.sprite.img ,Player_start_pos.y * 10,Player_start_pos.x * 10);
